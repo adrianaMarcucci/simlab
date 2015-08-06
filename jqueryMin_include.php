7b74452b28200ec86bd6ae4de1fc8b7763a7f2b8
adrianaMarcucci <adriana.marcucci@gmail.com>
2015-08-06 12:18:52 +0200

    Revert "files_2015_08_06"
    This reverts commit c995177ff6234b1ab218dee2790d3cb3219aa517.

--- jqueryMin_include.php
+++
@@ -1,7 +0,0 @@
-<!-- Include Jquery here in a script tag -->
-<!--[if lt IE 9]>
-	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
-<![endif]-->
-<!--[if gte IE 9]><!-->
-	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-<!--<![endif]-->
